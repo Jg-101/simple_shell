58b44fbd11f9a67eda85563690fbbed2582b4d87
root <jghariokwu@gmail.com>
2023-05-15 10:21:41 -0700

    sonho

--- _atoi.c
+++ _atoi.c
@@ -12,34 +12,6 @@ int interactive(info_t *info)
 }
 
 /**
- * is_delim - checks if character is a delimeter
- * @a: the char to check
- * @delim: the delimeter string to be used
- * Return: 1 if true,and 0 if false
- */
-int is_delim(char a, char *delim)
-{
-	while (*delim)
-		if (*delim++ == a)
-			return (1);
-	return (0);
-}
-
-/**
- *_isalpha - checks for alphabetic char
- *@a: The char to put in
- *Return: 1 if a is alphabetic, and 0 if false
- */
-
-int _isalpha(int a)
-{
-	if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
-
-/**
  *_atoi - converts a str to an int
  *@r: the str to be converted
  *Return: 0 if no numbers are in str, show converted num if not
@@ -71,4 +43,35 @@ int _atoi(char *r)
 		out = res;
 
 	return (out);
+}
+
+/**
+ * is_delim - checks if character is a delimeter
+ * @a: the char to check
+ * @delim: the delimeter string to be used
+ * Return: 1 if true,and 0 if false
+ */
+int is_delim(char a, char *delim)
+{
+	while (*delim)
+		if (*delim++ == a)
+			return (1);
+	return (0);
+}
+
+
+
+
+/**
+ *_isalpha - checks for alphabetic char
+ *@a: The char to put in
+ *Return: 1 if a is alphabetic, and 0 if false
+ */
+
+int _isalpha(int a)
+{
+	if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
